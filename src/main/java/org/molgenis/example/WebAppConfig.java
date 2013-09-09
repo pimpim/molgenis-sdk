c1046d4a3aacb5634149370a9587bb7eae69bcbc
dennishendriksen <d.hendriksen@umcg.nl>
2013-09-09 15:05:22 +0200

    Fix SDK

--- src/main/java/org/molgenis/example/WebAppConfig.java
+++ src/main/java/org/molgenis/example/WebAppConfig.java
@@ -1,29 +1,39 @@
 package org.molgenis.example;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.molgenis.DatabaseConfig;
 import org.molgenis.framework.db.Database;
 import org.molgenis.framework.security.Login;
 import org.molgenis.framework.server.MolgenisPermissionService;
 import org.molgenis.framework.server.MolgenisSettings;
+import org.molgenis.framework.ui.MolgenisPlugin;
 import org.molgenis.omx.auth.OmxPermissionService;
+import org.molgenis.ui.MolgenisPluginInterceptor;
 import org.molgenis.ui.MolgenisUi;
 import org.molgenis.ui.XmlMolgenisUi;
 import org.molgenis.ui.XmlMolgenisUiLoader;
 import org.molgenis.util.ApplicationContextProvider;
 import org.molgenis.util.AsyncJavaMailSender;
+import org.molgenis.util.GsonHttpMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 @Configuration
 @EnableWebMvc
@@ -43,12 +53,6 @@ public class WebAppConfig extends WebMvcConfigurerAdapter
 	private MolgenisSettings molgenisSettings;
 
 	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer)
-	{
-		configurer.enable("front-controller");
-	}
-
-	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry)
 	{
 		registry.addResourceHandler("/css/**").addResourceLocations("/css/", "classpath:/css/");
@@ -56,9 +60,28 @@ public class WebAppConfig extends WebMvcConfigurerAdapter
 		registry.addResourceHandler("/js/**").addResourceLocations("/js/", "classpath:/js/");
 	}
 
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters)
+	{
+		converters.add(new GsonHttpMessageConverter());
+		converters.add(new BufferedImageHttpMessageConverter());
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry)
+	{
+		String pluginInterceptPattern = MolgenisPlugin.PLUGIN_URI_PREFIX + "**";
+		registry.addInterceptor(molgenisPluginInterceptor()).addPathPatterns(pluginInterceptPattern);
+	}
+
+	@Bean
+	public MolgenisPluginInterceptor molgenisPluginInterceptor()
+	{
+		return new MolgenisPluginInterceptor(login, molgenisPermissionService(), molgenisUi());
+	}
+
 	/**
-	 * Bean that allows referencing Spring managed beans from Java code which is
-	 * not managed by Spring
+	 * Bean that allows referencing Spring managed beans from Java code which is not managed by Spring
 	 * 
 	 * @return
 	 */
@@ -66,6 +89,38 @@ public class WebAppConfig extends WebMvcConfigurerAdapter
 	public ApplicationContextProvider applicationContextProvider()
 	{
 		return new ApplicationContextProvider();
+	}
+
+	@Bean
+	public ApplicationListener<?> databasePopulator()
+	{
+		return new WebAppDatabasePopulator();
+	}
+
+	/**
+	 * Enable spring freemarker viewresolver. All freemarker template names should end with '.ftl'
+	 */
+	@Bean
+	public ViewResolver viewResolver()
+	{
+		FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+		resolver.setCache(true);
+		resolver.setSuffix(".ftl");
+
+		return resolver;
+	}
+
+	/**
+	 * Configure freemarker. All freemarker templates should be on the classpath in a package called 'freemarker'
+	 */
+	@Bean
+	public FreeMarkerConfigurer freeMarkerConfigurer()
+	{
+		FreeMarkerConfigurer result = new FreeMarkerConfigurer();
+		result.setPreferFileSystemAccess(false);
+		result.setTemplateLoaderPath("classpath:/templates/");
+
+		return result;
 	}
 
 	@Bean
